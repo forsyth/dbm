25a545d5d07ee4d0652308a507cbe4058a3bebcb
Charles Forsyth <charles.forsyth@gmail.com>
2026-05-25 18:55:33 +0100

    check Fetch immediatel after Store; run through all the keys when done

--- dbm_test.go
+++ dbm_test.go
@@ -2,7 +2,8 @@ package dbm_test
 
 import (
 	"bufio"
-	"fmt"
+	"bytes"
+	"io"
 	"os"
 	"strings"
 	"testing"
@@ -13,6 +14,14 @@ import (
 const (
 	wordsFile = "testdata/words"
 )
+
+func linesReader(f io.Reader, line func(lno int, key string, err error)) {
+	lines := bufio.NewScanner(f)
+	for lno := 1; lines.Scan(); lno++ {
+		key := strings.TrimSpace(lines.Text())
+		line(lno, key, nil)
+	}
+}
 
 func TestDBM(t *testing.T) {
 	// load
@@ -25,18 +34,47 @@ func TestDBM(t *testing.T) {
 	if err != nil {
 		t.Fatalf("cannot create testdata/db1: %s", err)
 	}
-	lines := bufio.NewScanner(words)
+	defer db.Close()
+	vals := make(map[string][]byte)
 	val := []byte{}
-	for lines.Scan() {
-		key := strings.TrimSpace(lines.Text())
+	linesReader(words, func(lno int, key string, err error) {
+		if len(key) == 0 {
+			t.Errorf("line %d: empty key", lno)
+			return
+		}
 		val = append(val, 'a')
 		if len(val) > 200 {
 			val = []byte{}
 		}
-		err := db.Store([]byte(key), val, false)
+		err = db.Store([]byte(key), val, false)
 		if err != nil {
-			t.Errorf("adding key %s (data len %d), want success; got %s", key, len(val), err)
+			t.Errorf("line %d: adding key %q (data len %d), want success; got %s", lno, key, len(val), err)
+			return
 		}
-		fmt.Printf("%s\n", key)
+		gb, err := db.Fetch([]byte(key))
+		if err != nil {
+			t.Errorf("line %d: refetch key %q: want success; got %s", lno, key, err)
+			return
+		}
+		if !bytes.Equal(gb, val) {
+			t.Errorf("line %d: refetch key %q: want %q; got %q", lno, key, val, gb)
+		}
+		vals[key] = val
+	})
+	t.Logf("LOAD OK")
+	_, err = words.Seek(0, io.SeekStart)
+	if err != nil {
+		t.Fatalf("cannot rewind %s: %s", wordsFile, err)
+		return
 	}
+	linesReader(words, func(lno int, key string, err error) {
+		gb, err := db.Fetch([]byte(key))
+		if err != nil {
+			t.Errorf("key %q fetch want success; got %s", key, err)
+			return
+		}
+		if !bytes.Equal(gb, vals[key]) {
+			t.Errorf("key %q fetch want %q; got %q", key, vals[key], gb)
+		}
+	})
 }
